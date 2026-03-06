a47a21d95c5a435dab839a6a83e49849979fc22a
rakoon19 <rahatakondo18@gmail.com>
2026-03-06 22:38:29 +0600

    signin page to all page

---
+++ js/signin.js
@@ -0,0 +1,11 @@
+const usernameInput = document.getElementById('username-input');
+const passwordInput = document.getElementById('password-input');
+const signinBTN = document.getElementById('signin-btn');
+
+signinBTN.addEventListener('click', () => {
+    let username = usernameInput.value;
+    let password = passwordInput.value;
+    if( username === 'admin' && password === 'admin123'){
+        window.location.replace('all.html');
+    }
+});
