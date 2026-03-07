3d32ff89ff9ee2aa7fd441c75f245394f04dee08
rakoon19 <rahatakondo18@gmail.com>
2026-03-07 06:23:16 +0600

    added responsiveness to all html and finished designing

---
+++ js/all.js
@@ -0,0 +1,11 @@
+// now i need to make cards and using api need to insert it in the cards id div
+
+// need to change cards numbah in the status dynamically
+
+// gotta function all the btns to work
+
+// need to work with the search option
+
+// have to work with modals
+
+// new issue btn optional i think
